ad7cab256629695c9cb675abaf650b65de5e5fc4
Subhajeet Muhuri <kenny3fcb@gmail.com>
2018-07-15 11:34:07 +0530

    aarch64--glibc--bleeding-edge-2018.06-1

---
+++ lib/gcc/aarch64-buildroot-linux-gnu/8.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/opt/aarch64--glibc--bleeding-edge-2018.06-1 --sysconfdir=/opt/aarch64--glibc--bleeding-edge-2018.06-1/etc --enable-static --target=aarch64-buildroot-linux-gnu --with-sysroot=/opt/aarch64--glibc--bleeding-edge-2018.06-1/aarch64-buildroot-linux-gnu/sysroot --disable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --with-gmp=/opt/aarch64--glibc--bleeding-edge-2018.06-1 --with-mpc=/opt/aarch64--glibc--bleeding-edge-2018.06-1 --with-mpfr=/opt/aarch64--glibc--bleeding-edge-2018.06-1 --with-pkgversion='Buildroot 2018.08-git-00465-g4f8d7c8' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --enable-tls --disable-libmudflap --enable-threads --without-isl --without-cloog --disable-decimal-float --with-abi=lp64 --with-cpu=cortex-a53 --enable-languages=c,c++ --with-build-time-tools=/opt/aarch64--glibc--bleeding-edge-2018.06-1/aarch64-buildroot-linux-gnu/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64" }, { "cpu", "cortex-a53" } };
